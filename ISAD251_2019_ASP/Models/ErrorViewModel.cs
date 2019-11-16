4990df98411313a96cf37824a2faefb88b2cdbbe
s1atkinson <shirley.atkinson@plymouth.ac.uk>
2019-11-16 19:33:42 +0000

    Created new application, adjusted template to match storyboard using Bootstrap.  Created Request controller and view.

---
+++ ISAD251_2019_ASP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ISAD251_2019_ASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
