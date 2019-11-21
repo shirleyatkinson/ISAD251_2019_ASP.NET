f40b99164cce4c3f2a484601c951e8df4b8d4790
shirleyatkinson <shirley.atkinson@plymouth.ac.uk>
2019-11-21 10:44:11 +0000

    Created drop down list from database table.  Request view now shows items from shModule.

--- ISAD251_2019_ASP/Controllers/RequestController.cs
+++ ISAD251_2019_ASP/Controllers/RequestController.cs
@@ -12,10 +12,22 @@ namespace ISAD251_2019_ASP.Controllers
 {
     public class RequestController : Controller
     {
+        private readonly ISAD251_SAtkinsonContext _context;
+
+        public RequestController(ISAD251_SAtkinsonContext context)
+        {
+            _context = context;
+        }
        
         public IActionResult Index()
         {
-           
+            IEnumerable<SelectListItem> ModuleList = (from p in _context.ShModule.AsEnumerable()
+                                                      select new SelectListItem
+                                                      {
+                                                          Text = p.Code,
+                                                          Value = p.ModuleId.ToString()
+                                                      }).ToList();
+            ViewBag.Modules = ModuleList;
             return View();
         }
 
